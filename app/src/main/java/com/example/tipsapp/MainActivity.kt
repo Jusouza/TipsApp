3582dca52184ada06e102d57601c3e4a2f36044c
Jusouza <jujussouza19@gmail.com>
2025-06-11 12:41:27 -0300

    improving UI/UX

--- app/src/main/java/com/example/tipsapp/MainActivity.kt
+++ app/src/main/java/com/example/tipsapp/MainActivity.kt
@@ -2,26 +2,15 @@ package com.example.tipsapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tipsapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    private var percentage: Int = 0
-    private var numOfPeopleSelected: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,17 +24,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        spinnerAdapterConfig()
-
-        binding.rgPercentage.setOnCheckedChangeListener { _, checkedId ->
-            percentage = when (checkedId) {
-                R.id.rb_option_one -> 10
-                R.id.rb_option_two -> 15
-                R.id.rb_option_three -> 20
-                else -> 0
-            }
-        }
-
         binding.btnClean.setOnClickListener{
             clearFields()
         }
@@ -55,47 +33,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun spinnerAdapterConfig(){
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.num_people,
-            android.R.layout.simple_spinner_item
-        )
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerNumberOfPeople.adapter = adapter
-
-
-        binding.spinnerNumberOfPeople.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                numOfPeopleSelected = position
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-        }
-    }
 
     private fun calculateTips(){
         val totalTableTemp = binding.tieTotal.text
+        val totalPeopleTemp = binding.tieNumberPeople.text
+        val totalPercentageTemp = binding.tiePercentage.text
 
-        if (totalTableTemp?.isEmpty() == true ){
+        if (totalTableTemp?.isEmpty() == true ||
+            totalPeopleTemp?.isEmpty() == true ||
+            totalPercentageTemp?.isEmpty() == true){
             Snackbar
                 .make(binding.tieTotal, getString(R.string.fill_fields), Snackbar.LENGTH_LONG)
                 .show()
         }
         else {
             val totalTable: Float = totalTableTemp.toString().toFloat()
-            val nPeople: Int = numOfPeopleSelected
+            val nPeople: Int = totalPeopleTemp.toString().toInt()
+            val percentageTips: Int = totalPercentageTemp.toString().toInt()
 
             val totalTemp = totalTable / nPeople
-            val tips = totalTemp * percentage / 100
+            val tips = totalTemp * percentageTips / 100
             val totalWithTips = totalTemp + tips
 
 
@@ -103,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             intent.apply {
                 putExtra("totalTable", totalTable)
                 putExtra("numPeople", nPeople)
-                putExtra("percentage", percentage)
+                putExtra("percentage", percentageTips)
                 putExtra("totalAmount", totalWithTips)
             }
             clearFields()
@@ -113,8 +70,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun clearFields() = with(binding) {
         tieTotal.setText("")
-        rgPercentage.clearCheck()
-        spinnerNumberOfPeople.setSelection(0)
+        tiePercentage.setText("")
+        tieNumberPeople.setText("")
     }
 
 }
